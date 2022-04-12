3d9c31fffbb00d24a0569584e0d430a99ae0ec16
Raj Gedhada <grajnikanth@gmail.com>
2022-04-12 11:44:09 -0700

    Added function to store HTTP request data into buffer of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // Building a web server using RUst
 
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // Server will listen at address 127.0.0.1:7878
@@ -9,8 +11,23 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection to web server established");
+        handle_connection(stream);
     }
 
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // createa buffer to hold 1024 bytes of data on stack
+    // mut because the stream from TCP connection might keep changing
+    let mut buffer = [0; 1024];
+
+    // read the TCP stream data -(http request) and store it in buffer 
+    stream.read(&mut buffer).unwrap();
+
+    // Convert the buffer bytes to string. from_utf..() function takes &[u8]
+    // lossy part is going to replaces an invalid sequence with "replacement character"
+    // The below will print the request sent by a browser
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
 }
